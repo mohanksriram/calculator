1a88815f80e6eb34b8efddee91fa14d2421eb084
Mohan Kumar Sriram <mohankumarsriram7@gmail.com>
2021-06-09 19:52:30 +0530

    added bazel build

---
+++ calculator/calculator.cc
@@ -0,0 +1,8 @@
+// A Scientific Calculator
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << "Welcome to Calculator!" << "\n";
+    std::cout << "Please enter the expression to compute" << "\n";    
+}
