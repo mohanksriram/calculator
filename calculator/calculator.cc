d8823d0f1f2fcf4c3dc77c226da4ca001cc7520a
Mohan Kumar Sriram <mohankumarsriram7@gmail.com>
2021-06-15 21:33:44 +0530

    added support for mode selection: infix or postfix

--- calculator/calculator.cc
+++ calculator/calculator.cc
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <exception>
 #include <iostream>
+#include <limits>
 #include <string>
 
 #include "parser/parser.h"
@@ -9,26 +10,36 @@
 
 int main() {
     std::cout << "Welcome to Calculator!" << "\n";
-    std::cout << "Please enter the expression to compute" << "\n";
+    std::cout << "Please enter the mode of operation " << "\n";
+    std::cout << "1: Infix or 2: Postfix" << "\n";
+    std::string mode;
+    std::cin >> mode;
+    std::cout << "Please enter the expression to compute" << std::endl;
+    std::cin >> std::ws;
 
     std::string expr;
     std::getline(std::cin, expr);
 
     CalculatorParser parser;
     Evaluator evaluator;
-    
+    double res = 0.0;
     try {
         std::deque<Token> tokens = parser.tokenize(expr);
-        std::deque<Token> rpn_tokens = parser.rpn(tokens);
+        if(mode == "1") {
+            std::deque<Token> rpn_tokens = parser.rpn(tokens);
+            for(int i = 0, size = rpn_tokens.size(); i < size; ++i) {
+                std::cout << rpn_tokens[i].str << (i < size-1 ? " ": "\n"); 
+            }
+            res = evaluator.evaluate(rpn_tokens);
+        } else {
+            res = evaluator.evaluate(tokens);
+        }
+        
+        std::cout << "res: " << res << std::endl;
 
         // for(const Token& token: tokens) {
         //     std::cout << token.str << std::endl;
         // }
-        // for(int i = 0, size = rpn_tokens.size(); i < size; ++i) {
-        //     std::cout << rpn_tokens[i].str << (i < size-1 ? " ": "\n"); 
-        // }
-        double res = evaluator.evaluate(rpn_tokens);
-        std::cout << "res: " << res << std::endl;
     }
     catch (...) {
         try {
